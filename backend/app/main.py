0097634e3e6a1684fe7316ff4723cb511269f91e
hooiv <125276621+hooiv@users.noreply.github.com>
2025-06-12 01:53:07 +0530

    Changes to be committed: 	modified:   backend/app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
-from routes import router
+from .routes import router
 
 # Load environment variables
 load_dotenv()
