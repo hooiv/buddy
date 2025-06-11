98ae3e12dc231ac26623618f4abfc4ae0ff848ff
hooiv <125276621+hooiv@users.noreply.github.com>
2025-06-12 02:01:01 +0530

    Changes to be committed: 	modified:   backend/app/main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173","https://buddy-flame-phi.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173","https://buddy-flame-phi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
